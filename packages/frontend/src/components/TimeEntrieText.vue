cc13f1d5553313e0bd92b7188fcf9a1308ab2ac7
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2021-12-09 21:13:28 +0100

    WIP - expand on click DOES NOT WORK

--- packages/frontend/src/components/TimeEntrieText.vue
+++ packages/frontend/src/components/TimeEntrieText.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="entry">
-    <p class="customer-name">{{ task.project.customer.name }}</p>
-    <div class="activity-name-container">
-      <p class="activity-name">{{ task.name }} {{ task.project.name }}</p>
-      <small class="rate-text">{{ compensationRatePercentage }}</small>
+  <div class="entry">  
+    <div v-if="!expanded" class="entry-small" v-on:click="expandEntryToggle">
+      <p class="customer-name">{{ task.project.customer.name }} - {{ task.project.name }} super duper hurra super duper hurra super duper hurra super duper hurra</p>
+      <!-- <p class="customer-name">{{ task.project.name }}</p> -->
+      <div class="activity-name-container">
+        <span class="activity-name">{{ task.name }} super duper hurra super duper hurra super duper hurra super duper hurra</span>
+        <small class="rate-text">{{ compensationRatePercentage }}</small>
+      </div>
     </div>
-    <div v-show="showPadlock" class="padlock">
-      <md-icon class="icon">lock</md-icon>
+    <div v-if="expanded" class="entry-expanded" v-on:click="expandEntryToggle">
+      <label>
+        Kunde:
+      <span class="entryText">{{ task.project.customer.name }}</span>
+      </label>
+      <label>
+          Prosject:
+      <span class="entryText">{{ task.project.name }} super duper hurra super duper hurra super duper hurra super duper hurra</span>
+      </label>
+      <label>
+      Aktivitet:
+      <span class="entryText activity-name">{{ task.name }} super duper hurra super duper hurra super duper hurra super duper hurra</span>
+      </label>
+      <label>
+        Rate:
+      <small class="entryText">{{ compensationRatePercentage }}</small>
+      </label>  
     </div>
+      <div v-show="showPadlock" class="padlock">
+        <md-icon class="icon">lock</md-icon>
+      </div>
   </div>
 </template>
 
@@ -16,6 +37,11 @@ import Vue from "vue";
 import { Task } from "@/store/tasks";
 
 export default Vue.extend({
+  data() {
+    return {
+      expanded: false
+    }
+  },
   props: {
     task: {
       type: Object as () => Task,
@@ -23,6 +49,11 @@ export default Vue.extend({
         return {} as Task;
       },
     },
+  },
+  methods: {
+    expandEntryToggle() {
+      this.expanded = !this.expanded
+    }
   },
   computed: {
     compensationRatePercentage(): string {
@@ -40,10 +71,30 @@ export default Vue.extend({
   white-space: nowrap;
   overflow: hidden;
   font-size: 0.8rem;
+  display: grid;
+  grid-template-columns: 1fr 30px;
+}
+
+label {
+  display: block;
+  font-weight: 600;
+  font-size: 0.6rem;
+}
+
+.entryText {
+  font-weight: normal;
+  font-size: 0.7rem;
 }
 
 .customer-name {
   margin: 0;
+  font-size: 0.6rem;
+
+  width: calc(90%);
+  display: inline-block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 
 .activity-name-container {
@@ -58,6 +109,9 @@ export default Vue.extend({
   font-weight: 600;
   margin: 0;
 
+  width: calc(90%);
+  display: inline-block;
+  white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
 }
@@ -67,12 +121,20 @@ export default Vue.extend({
   margin-left: 0.25rem;
 }
 
-.padlock {
+.padlock.expanded {
   margin-right: 0.4rem;
   float: right;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
 }
+
+.padlock {
+  display: grid;
+}
+
 .icon {
-  position: relative;
-  bottom: 0.5rem;
+  justify-self: center;
 }
+
 </style>
